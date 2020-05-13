33710c201757cba8bcfbbb65edda5edecd155939
Sabo <lightspeedkey@yahoo.com>
2020-05-12 22:22:03 -0400

    added some spacing for navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,11 +4,31 @@ import {AppBar, Toolbar, Box, Badge, Button} from '@material-ui/core'
 import {ShoppingCart} from '@material-ui/icons'
 import {connect} from 'react-redux'
 import {makeStyles} from "@material-ui/core/styles"
+import Typography from '@material-ui/core/Typography';
 
+
+const useStyles = makeStyles((theme) =>({
+    root:{
+        flexGrow : 1,
+    },
+    title: {
+        flexGrow: 1,
+        marginLeft : theme.spacing(8)
+    },
+    men:{
+        marginRight : theme.spacing(3)
+
+    },
+    cart:{
+        marginRight : theme.spacing(10)
+    }
+
+}))
 
 function NavBar(props){
+    const classes = useStyles()
     return(
-        <div>
+        <div className ={classes.root}>
             {/* <h3> logo</h3>
             <ul>
                <Link to = "/about"> <li>About</li> </Link>
@@ -20,18 +40,22 @@ function NavBar(props){
                 <Toolbar>
                    
                    {/* <Button component = {Link} to = "/cart"> */}
-                   <Link to ="/cart">
-                   <Badge badgeContent = {props.value.totalquantity} color = "secondary">
-                    <ShoppingCart ></ShoppingCart>
-                    </Badge>
-                    </Link>
+                   <Typography variant="h6" className={classes.title}>
+           <Link to = '/home'>TMstore </Link> 
+          </Typography>
 
                     <Link to = '/about'>
                         About
                     </Link>
 
-                    <Link to = '/mens'>
+                    <Link className= {classes.men} to = '/mens'>
                         Mens
+                    </Link>
+
+                    <Link className = {classes.cart}to ="/cart">
+                   <Badge badgeContent = {props.value.totalquantity} color = "secondary">
+                    <ShoppingCart ></ShoppingCart>
+                    </Badge>
                     </Link>
                     {/* </Button> */}
                      
